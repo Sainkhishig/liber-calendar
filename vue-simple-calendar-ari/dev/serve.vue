38e39674794e3e8b054b10aa8b9c1181315efb16
unknown <ari.ariuka67@gmail.com>
2021-07-15 15:26:54 +0900

    ts set

---
+++ vue-simple-calendar-ari/dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import VueSimpleCalendarAri from '@/vue-simple-calendar-ari.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    VueSimpleCalendarAri
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-simple-calendar-ari />
+  </div>
+</template>
